2b7d23d1eaa4c80b0ad4701b98613a288af5f32f
Tram Vuong <tvuong@Jonathans-Air.lan>
2022-10-14 23:32:42 -0700

    add code for form and api

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,37 @@
 /** Exercise 03 - Form * */
-const name = document.querySelector('#name')
+const inputName = document.querySelector('#name')
 const email = document.querySelector('#email')
+const message = document.querySelector('#message')
+const newsletter = document.querySelector('#newsletter')
+const submit = document.querySelector('#submit');
+
+const checkMessage = (message) => {
+    if (message.length !== 0) {
+        return message
+    } else {
+        return 'No feedback was submitted.'
+    }
+}
+
+const checkNewsletter = (newsletter) => {
+    if (newsletter.checked) {
+        return 'Yes, I would like to join the newsletter.'
+    } else {
+        return 'No, thank you.'
+    }
+}
+
+const handler = () => {
+    if (inputName.value.length !== 0 && email.value.length !== 0) {
+        let result = '========= Form Submission =========\n'
+
+        result = result.concat('\t', 'Name: ', inputName.value, '\n')
+        result = result.concat('\t', 'Email: ', email.value, '\n')
+        result = result.concat('\t', 'Feedback: ', checkMessage(message.value), '\n')
+        result = result.concat('\t', 'Newsletter: ', checkNewsletter(newsletter), '\n')
+
+        console.log(result)
+    }
+}
+
+submit.addEventListener('click', handler)
--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,27 @@
 /** Exercise 04 - API **/
 
-const url = 'https://restcountries.com/v3.1/all';
+const url = 'https://restcountries.com/v2/all'
+const results = document.querySelector('#results')
 
-// Add your code here
+const getData = (url) => {
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            data.next != null && getData(data.next)
+            return data
+        })
+        .then((countries) => {
+            countries.forEach((country) => {
+                let countryItem = document.createElement('li')
+                
+                countryItem.innerText = `${country.name} - ${country.population.toLocaleString()}`
+                countryItem.setAttribute('class', 'list-item');
+                results.append(countryItem)
+            })
+        })
+        .catch((error) => console.error(error))
+}
 
 getData(url);
